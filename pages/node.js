01fd2bda9df0bebebc5f3a197a960ec627973f1f
Summit smith <54981042+summitsmith@users.noreply.github.com>
2021-06-08 15:32:41 +0300

    Add files via upload

---
+++ pages/node.js
@@ -0,0 +1,34 @@
+function access(){
+	let name=document.getElementById("name").value;
+	let password=document.getElementById("pass").value;	
+    if((name=="S1/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S3/2021" && password=="demo")||(name=="S5/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")
+    ||(name=="S2/2021" && password=="demo")||(name=="S2/2021" && password=="demo")){
+	window.open("home.html");
+	}
+	else{
+    alert("Wrong Unique User ID and password combinations");
+}
+}
