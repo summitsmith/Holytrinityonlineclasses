83eb45119131d194933a64c640823f51e1001f27
Summit smith <54981042+summitsmith@users.noreply.github.com>
2021-06-20 20:21:07 +0300

    Update node.js

--- pages/node.js
+++ pages/node.js
@@ -1,33 +1,33 @@
 function access(){
 	let name=document.getElementById("name").value;
 	let password=document.getElementById("pass").value;	
-    if((name=="S1/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S3/2021" && password=="holy")||(name=="S5/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")
-    ||(name=="S2/2021" && password=="holy")||(name=="S2/2021" && password=="holy")){
+    if((name=="S3/070" && password=="lilian")||(name=="S3/071" && password=="edward")
+    ||(name=="S3/072" && password=="flavia")||(name=="S3/073" && password=="ian")
+    ||(name=="S3/074" && password=="olivia")||(name=="S3/075" && password=="jamira")
+    ||(name=="S3/076" && password=="charles")||(name=="S3/077" && password=="fahad")
+    ||(name=="S3/078" && password=="betty")||(name=="S3/079" && password=="atrabah")
+    ||(name=="S3/080" && password=="ivan")||(name=="S3/081" && password=="frolence")
+    ||(name=="S3/082" && password=="devis")||(name=="S3/083" && password=="phionah")
+    ||(name=="S3/084" && password=="ssekanjako")||(name=="S3/085" && password=="gonzaga")
+    ||(name=="S3/086" && password=="benard")||(name=="S3/087" && password=="brain")
+    ||(name=="S3/088" && password=="winnie")||(name=="S3/089" && password=="jamirah")
+    ||(name=="S3/090" && password=="maria")||(name=="S3/091" && password=="ritah")
+    ||(name=="S3/092" && password=="nakimera")||(name=="S3/093" && password=="kateregga")
+    ||(name=="S3/094" && password=="lule")||(name=="S3/095" && password=="frank")
+    ||(name=="S3/096" && password=="benita")||(name=="S3/097" && password=="agnes")
+    ||(name=="S3/098" && password=="geofrey")||(name=="S3/099" && password=="ibra")
+    ||(name=="S3/100" && password=="shaban")||(name=="S3/101" && password=="evelyne")
+    ||(name=="S3/102" && password=="nambi")||(name=="S3/103" && password=="john")
+    ||(name=="S3/104" && password=="paul")||(name=="S3/105" && password=="jane")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")
+    ||(name=="S3/2021" && password=="holy")||(name=="S3/2021" && password=="holy")){
 	window.open("home.html");
-	}else if(name=="trinity" && password=="trinity"){
+	}else if(name=="default" && password=="default"){
         window.open("home.html");
     }
 	else{
