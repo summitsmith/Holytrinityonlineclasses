1837d9d15e46f3b1a275237e61fb61d036c0c662
Summit smith <54981042+summitsmith@users.noreply.github.com>
2021-08-25 00:18:17 -0700

    Update node.js

--- pages/node.js
+++ pages/node.js
@@ -1,7 +1,7 @@
 function access(){
 	let name=document.getElementById("name").value;
 	let password=document.getElementById("pass").value;	
-    if((name=="S3/074" && password=="lilian")||(name=="S3/2021" && password=="holy")){
+    if((name=="S3/074" && password=="lilian")||(name=="S1/stud031" && password=="kemigisha")){
 	window.open("home.html");
 	}else if(name=="trinity" && password=="trinity"){
         window.open("home.html");
