9b514a005ee6ac66098c32c0592a983de68b010d
Summit smith <54981042+summitsmith@users.noreply.github.com>
2021-08-30 11:05:35 +0300

    Add files via upload

--- pages/node.js
+++ pages/node.js
@@ -1,9 +1,9 @@
 function access(){
 	let name=document.getElementById("name").value;
 	let password=document.getElementById("pass").value;	
-    if((name=="S3/074" && password=="lilian")||(name=="S1/stud031" && password=="kemigisha")){
+    if((name=="stud032" && password=="nambi")||(name=="S1/stud031" && password=="kemigisha")){
 	window.open("home.html");
-	}else if(name=="trinity" && password=="trinity"){
+	}else if(name=="holy" && password=="trinity"){
         window.open("home.html");
     }
 	else{
