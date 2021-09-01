d115e80af7c44ff58b4bd38a921ccce53c14a7fd
Summit smith <54981042+summitsmith@users.noreply.github.com>
2021-09-01 04:05:56 -0700

    Update node.js

--- pages/node.js
+++ pages/node.js
@@ -1,12 +1,13 @@
 function access(){
 	let name=document.getElementById("name").value;
 	let password=document.getElementById("pass").value;	
-    if((name=="stud032" && password=="nambi")||(name=="S1/stud031" && password=="kemigisha")){
+    if((name=="stud032" && password=="nambi")||(name=="S1/stud031" && password=="kemigisha")||(name=="stud033" && password=="kawooya")){
 	window.open("home.html");
-	}else if(name=="holy" && password=="trinity"){
+	}else if(name=="holy" && password=="trin"){
         window.open("home.html");
     }
 	else{
     alert("Wrong Unique User ID and password combinations");
 }
 }
+
